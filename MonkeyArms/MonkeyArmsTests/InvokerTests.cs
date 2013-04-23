e46c88abafbcdfc119d1c46315af559aee1b5777
Ben Bishop <ben@freckleinteractive.net>
2013-04-22 20:01:39 -0500

    Added mscorlib.dll to act as a stop gap until PCLs get full support.

---
+++ MonkeyArms/MonkeyArmsTests/InvokerTests.cs
@@ -0,0 +1,19 @@
+using System;
+using NUnit.Framework;
+using MonkeyArms;
+
+namespace MonkeyArmsTests
+{
+	[TestFixture()]
+	public class InvokerTests
+	{
+		protected Invoker TestInvoker = new Invoker();
+
+		[Test()]
+		public void TestCase ()
+		{
+
+		}
+	}
+}
+
