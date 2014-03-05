25462faef28767b6b416d1c819742cf558028b7a
Ben Bishop <ben@rendr.io>
2014-03-04 19:16:58 -0600

    Added InjectionMap InjectionMap will allow developers the ability to specify injection rules with teh DIUtil.

---
+++ MonkeyArms/MonkeyArmsPCL/InjectionMap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MonkeyArms
+{
+	public class InjectionMap
+	{
+		public delegate object ValueDelegate ();
+
+		protected Dictionary<Type, ValueDelegate> InvokerMap = new Dictionary<Type, ValueDelegate> ();
+
+		public InjectionMap ()
+		{
+	
+		}
+
+		public void Add <TValueType> (ValueDelegate par) where TValueType:class
+		{
+			InvokerMap [typeof(TValueType)] = par;
+		}
+
+		public object Get (Type valueType)
+		{
+
+			return InvokerMap [valueType] ();
+		}
+
+		public bool HasTypeMapped (Type typeToCheck)
+		{
+			return InvokerMap.ContainsKey (typeToCheck);
+		}
+	}
+}
+
---
+++ MonkeyArms/MonkeyArmsTests/InjectionMapTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using MonkeyArms;
+using Should;
+
+namespace MonkeyArmsTests
+{
+	[TestFixture ()]
+	public class InjectionMapTests
+	{
+		[Test (Description = "Injection map should allow a developer to specify what instance of a class type should be used")]
+		public void VerifyAddGet ()
+		{
+			TestClass testClassInstance = new TestClass ();
+			TestInjectionMap.HasTypeMapped (typeof(TestClass)).ShouldBeFalse ();
+			TestInjectionMap.Add<TestClass> (() => testClassInstance);
+			TestInjectionMap.HasTypeMapped (typeof(TestClass)).ShouldBeTrue ();
+			TestInjectionMap.Get (typeof(TestClass)).ShouldEqual (testClassInstance);
+
+		}
+
+		protected InjectionMap TestInjectionMap = new InjectionMap ();
+
+		public class TestClass
+		{
+		}
+	}
+}
+
