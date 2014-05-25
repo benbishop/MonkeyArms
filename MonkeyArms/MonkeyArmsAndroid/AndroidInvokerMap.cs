b828c6fa39bdffdfd7fb19c46b3a9af7589efaef
Ben Bishop <ben@rendr.io>
2014-05-25 08:42:47 -0500

    Added platform specific InvokerMaps to reduce use of InvokeOnMainThread and RunOnUIThread.

---
+++ MonkeyArms/MonkeyArmsAndroid/AndroidInvokerMap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MonkeyArms;
+using Android.App;
+using Java.Lang;
+
+namespace MonkeyArms.Android
+{
+	public class AndroidInvokerMap:InvokerMap
+	{
+		public AndroidInvokerMap ()
+		{
+		}
+
+		public override void Add (IInvoker targetInvoker, EventHandler handlerFunction, object handlerHost)
+		{
+			if (handlerHost is Activity) {
+				base.Add (targetInvoker, (object sender, EventArgs e) => ((Activity)handlerHost).RunOnUiThread (() => handlerFunction (sender, e)), handlerHost);
+			} else if (handlerHost is Fragment) {
+				base.Add (targetInvoker, (object sender, EventArgs e) => ((Fragment)handlerHost).Activity.RunOnUiThread (() => handlerFunction (sender, e)), handlerHost);
+			} else {
+				base.Add (targetInvoker, handlerFunction, handlerHost);
+			}
+
+		}
+	}
+}
+
---
+++ MonkeyArms/MonkeyArmsIOS/IOSInvokerMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MonkeyArms;
+using MonoTouch.UIKit;
+
+namespace MonkeyArms.IOS
+{
+	public class IOSInvokerMap:InvokerMap
+	{
+		public IOSInvokerMap ()
+		{
+		}
+
+		public override void Add (IInvoker targetInvoker, EventHandler handlerFunction, object handlerHost)
+		{
+			if (handlerHost is UIViewController) {
+
+				base.Add (targetInvoker, (object sender, EventArgs e) => ((UIViewController)handlerHost).InvokeOnMainThread (() => handlerFunction (sender, e)), handlerHost);
+			} else if (handlerHost is UIView) {
+				base.Add (targetInvoker, (object sender, EventArgs e) => ((UIView)handlerHost).InvokeOnMainThread (() => handlerFunction (sender, e)), handlerHost);
+
+			} else {
+				base.Add (targetInvoker, handlerFunction, handlerHost);
+			}
+
+		}
+	}
+}
+
