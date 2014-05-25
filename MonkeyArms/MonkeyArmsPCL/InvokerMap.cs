f5b8c08a5df456f14ef0070e05f547bedde2c41f
Ben Bishop <ben@rendr.io>
2014-05-25 11:03:28 -0500

    Updated Add method to include Handler object param.

--- MonkeyArms/MonkeyArmsPCL/InvokerMap.cs
+++ MonkeyArms/MonkeyArmsPCL/InvokerMap.cs
@@ -3,61 +3,55 @@ using System.Collections.Generic;
 
 namespace MonkeyArms
 {
-    public class InvokerMap
-    {
-        protected Dictionary<IInvoker, List<EventHandler>> Map = new Dictionary<IInvoker, List<EventHandler>>();
+	public class InvokerMap
+	{
+		protected Dictionary<IInvoker, List<EventHandler>> Map = new Dictionary<IInvoker, List<EventHandler>> ();
 
-        public bool IsInvokerMapped(IInvoker targetInvoker)
-        {
-            return Map.ContainsKey(targetInvoker);
-        }
+		public bool IsInvokerMapped (IInvoker targetInvoker)
+		{
+			return Map.ContainsKey (targetInvoker);
+		}
 
-        public bool InvokerHasHandler(IInvoker targetInvoker, EventHandler handlerFunction)
-        {
-            return (IsInvokerMapped(targetInvoker) && Map[targetInvoker].Contains(handlerFunction));
-        }
+		public bool InvokerHasHandler (IInvoker targetInvoker, EventHandler handlerFunction)
+		{
+			return (IsInvokerMapped (targetInvoker) && Map [targetInvoker].Contains (handlerFunction));
+		}
 
-        public void Add(IInvoker targetInvoker, EventHandler handlerFunction)
-        {
-            if (!IsInvokerMapped(targetInvoker))
-            {
-                Map[targetInvoker] = new List<EventHandler>();
-            }
-            if (!InvokerHasHandler(targetInvoker, handlerFunction))
-            {
-                Map[targetInvoker].Add(handlerFunction);
-                targetInvoker.Invoked += handlerFunction;
-            }
-        }
+		public virtual void Add (IInvoker targetInvoker, EventHandler handlerFunction, object handlerHost = null)
+		{
+			if (!IsInvokerMapped (targetInvoker)) {
+				Map [targetInvoker] = new List<EventHandler> ();
+			}
+			if (!InvokerHasHandler (targetInvoker, handlerFunction)) {
+				Map [targetInvoker].Add (handlerFunction);
+				targetInvoker.Invoked += handlerFunction;
+			}
+		}
 
-        public void Remove(Invoker targetInvoker, EventHandler handlerFunction)
-        {
-            if (Map.ContainsKey(targetInvoker) && Map[targetInvoker].Contains(handlerFunction))
-            {
-                Map[targetInvoker].Remove(handlerFunction);
-                targetInvoker.Invoked -= handlerFunction;
-            }
-        }
+		public void Remove (Invoker targetInvoker, EventHandler handlerFunction)
+		{
+			if (Map.ContainsKey (targetInvoker) && Map [targetInvoker].Contains (handlerFunction)) {
+				Map [targetInvoker].Remove (handlerFunction);
+				targetInvoker.Invoked -= handlerFunction;
+			}
+		}
 
-        public void RemoveAll()
-        {
-            foreach (var keyValuePair in Map)
-            {
-                foreach (var eventHandler in keyValuePair.Value)
-                {
-                    keyValuePair.Key.Invoked -= eventHandler;
-                }
-                keyValuePair.Value.RemoveAll(e => true);
-            }
-        }
+		public void RemoveAll ()
+		{
+			foreach (var keyValuePair in Map) {
+				foreach (var eventHandler in keyValuePair.Value) {
+					keyValuePair.Key.Invoked -= eventHandler;
+				}
+				keyValuePair.Value.RemoveAll (e => true);
+			}
+		}
 
-        public int GetInvokerHandlerCount(Invoker targetInvoker)
-        {
-            if (Map.ContainsKey(targetInvoker))
-            {
-                return Map[targetInvoker].Count;
-            }
-            return 0;
-        }
-    }
+		public int GetInvokerHandlerCount (Invoker targetInvoker)
+		{
+			if (Map.ContainsKey (targetInvoker)) {
+				return Map [targetInvoker].Count;
+			}
+			return 0;
+		}
+	}
 }
